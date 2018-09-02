2638602defd72e3959b76ea934f38e5c7d397c3e
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-09-02 20:34:34 +0200

    bug fix

--- core50_utils/evaluat.py
+++ core50_utils/evaluat.py
@@ -1,57 +1,57 @@
 from distutils.version import StrictVersion
 
-
 import os
 import sys
 import tensorflow as tf
-from PIL import Image
 import glob
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
-os.chdir("images/core50_test")
 
-sys.path.append("..")
+#sys.path.append("..")
 from object_detection.utils import ops as utils_ops
 
 if StrictVersion(tf.__version__) < StrictVersion('1.9.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.9.* or later!')
 
-
-
+# For double checking we store predictions on .txt files:
+SAVE_LABELS = 'CHECK_LABELS.txt'
+SAVE_BBOX = 'CHECK_BBOX.txt'
 
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = 'frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data/', 'core_50_map.pbtxt')
+PATH_TO_LABELS = os.path.join('data', 'core_50_map.pbtxt')
 
 NUM_CLASSES = 50
 
 class_errs = 0
 localiz_errs = 0
+tot_images = 0
 
-
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-  od_graph_def = tf.GraphDef()
-  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
-    serialized_graph = fid.read()
-    od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
-
-# Loading label map
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+print("Loading label maps: "+PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# Helper code
+list_predicted_labels = []
+list_predicted_bbox = []
+file1 = open(SAVE_LABELS, 'w')
+file2 = open(SAVE_BBOX, 'w')
 
-# import the necessary packages
-import numpy as np
+def load_image_into_numpy_array(image_path):
+  image = Image.open(image_path)
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
 
 def bb_intersection_over_union(boxA, boxB):
     # determine the (x, y)-coordinates of the intersection rectangle
@@ -77,98 +77,86 @@ def bb_intersection_over_union(boxA, boxB):
     return iou
 
 
+def run_inference_for_images(images, graph):
+    with graph.as_default():
+        with tf.Session() as sess:
+            output_dict_array = []
 
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
+            for image in images:
+                # Get handles to input and output tensors
+                ops = tf.get_default_graph().get_operations()
+                all_tensor_names = {output.name for op in ops for output in op.outputs}
+                tensor_dict = {}
+                for key in [
+                    'num_detections', 'detection_boxes', 'detection_scores',
+                    'detection_classes', 'detection_masks'
+                ]:
+                    tensor_name = key + ':0'
+                    if tensor_name in all_tensor_names:
+                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                            tensor_name)
+                image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+                image2 = load_image_into_numpy_array(image)
+                # Run inference
+                output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image2, 0)})
+                # all outputs are float32 numpy arrays, so convert types as appropriate
+                output_dict['num_detections'] = str(output_dict['num_detections'][0])
+                output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8)
+                output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+                output_dict['detection_scores'] = output_dict['detection_scores'][0]
+                output_dict_array.append(output_dict)
+                global tot_images
+                tot_images = tot_images - 1
+                print('Remaining images: ' + str(tot_images))
 
-
-
-list_predicted_labels = []
-list_predicted_bbox = []
-
-
-
-def run_inference_for_single_image(image, graph):
-  with graph.as_default():
-    with tf.Session() as sess:
-      # Get handles to input and output tensors
-      ops = tf.get_default_graph().get_operations()
-      all_tensor_names = {output.name for op in ops for output in op.outputs}
-      tensor_dict = {}
-      for key in [
-          'num_detections', 'detection_boxes', 'detection_scores',
-          'detection_classes', 'detection_masks'
-      ]:
-        tensor_name = key + ':0'
-        if tensor_name in all_tensor_names:
-          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-              tensor_name)
-      if 'detection_masks' in tensor_dict:
-        # The following processing is only for single image
-        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-            detection_masks, detection_boxes, image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(
-            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-        # Follow the convention by adding back the batch dimension
-        tensor_dict['detection_masks'] = tf.expand_dims(
-            detection_masks_reframed, 0)
-      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
-      # Run inference
-      output_dict = sess.run(tensor_dict,
-                             feed_dict={image_tensor: np.expand_dims(image, 0)})
-
-      # all outputs are float32 numpy arrays, so convert types as appropriate
-      output_dict['num_detections'] = int(output_dict['num_detections'][0])
-      output_dict['detection_classes'] = output_dict[
-          'detection_classes'][0].astype(np.uint8)
-      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-      output_dict['detection_scores'] = output_dict['detection_scores'][0]
-      if 'detection_masks' in output_dict:
-        output_dict['detection_masks'] = output_dict['detection_masks'][0]
-  return output_dict
+    return output_dict_array
 
 
+# LOADING FROZEN GRAPH
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  print("Loading graph: "+PATH_TO_FROZEN_GRAPH)
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
 
-for image_path in sorted(glob.glob("*.jpg")):
-    image = Image.open(image_path)
-    # the array based representation of the image will be used later in order to prepare the
-    # result image with boxes and labels on it.
-    image_np = load_image_into_numpy_array(image)
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+# LOADING TEST SET, SORTED ALPHABETICALLY
+images = sorted(glob.glob("images/test/*.jpg"))
+tot_images=len(images)
+print("Test set dimension: "+str(tot_images)+" images")
+
+# ARRAY WITH DETECTION RESULTS
+output_dict_array = run_inference_for_images(images, detection_graph)
+
+# CHECKING RESULTS
+for idx in range(len(output_dict_array)):
+    output_dict = output_dict_array[idx]
+    image_np=load_image_into_numpy_array(images[idx])
     image_np_expanded = np.expand_dims(image_np, axis=0)
-    # Actual detection.
-    output_dict = run_inference_for_single_image(image_np, detection_graph)
-    # Visualization of the results of a detection.
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        image_np,
-        output_dict['detection_boxes'],
-        output_dict['detection_classes'],
-        output_dict['detection_scores'],
-        category_index,
-        instance_masks=output_dict.get('detection_masks'),
-        use_normalized_coordinates=True,
-        line_thickness=8)
+    #list_predicted_labels.append(vis_util.predicted_label)
     list_predicted_labels.append(vis_util.predicted_label)
-    width, height = image.size
+    #conversion to original coordinates
     ymin = int(vis_util.predicted_bbox[0] * 350)
     xmin = int(vis_util.predicted_bbox[1] * 350)
     ymax = int(vis_util.predicted_bbox[2] * 350)
     xmax = int(vis_util.predicted_bbox[3] * 350)
-    list_predicted_bbox.append(str(ymin)+','+str(xmin)+','+str(ymax)+','+str(xmax))
-    list_predicted_bbox.append(vis_util.predicted_bbox)
+
+    list_predicted_bbox.append(str(ymin) + ',' + str(xmin) + ',' + str(ymax) + ',' + str(xmax))
+    file1.write(vis_util.predicted_label+'\n')
+    file2.write(str(ymin) + ',' + str(xmin) + ',' + str(ymax) + ',' + str(xmax) + '\n')
+    print("Predicted class for " + images[idx] + " --> " + vis_util.predicted_label)
+    print("Predicted bbox for " + images[idx] + ": " + str(ymin) + ',' + str(xmin) + ',' + str(ymax) + ',' + str(xmax))
+
 
 #loading GT labels/bbox from .txt
-ground_truth_list_labels = open('ground_truth_labels2.txt').read().splitlines()
-ground_truth_list_bbox = open('ground_truth_bbox2.txt').read().splitlines()
+ground_truth_list_labels = open('images/ground_truth_labels2.txt').read().splitlines()
+ground_truth_list_bbox = open('images/ground_truth_bbox2.txt').read().splitlines()
+
+#saving txt files
+file1.close()
+file2.close()
 
 # check two lists are of the same length:
 if len(ground_truth_list_bbox) != len(list_predicted_bbox):
@@ -184,8 +172,8 @@ for f, b in zip(ground_truth_list_labels, list_predicted_labels):
 
 # calculate localization errors:
 for f, b in zip(ground_truth_list_bbox, list_predicted_bbox):
-    f=f.split(",")
-    b=b.split(",")
+    f = f.split(",")
+    b = b.split(",")
     if bb_intersection_over_union(list(map(int, f)),list(map(int, b))) > 0.5:
         localiz_errs = localiz_errs+1
 
